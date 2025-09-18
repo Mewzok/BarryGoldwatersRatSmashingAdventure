32916cb1fc94f3b7f008bfe96ac71504e1666a66
404kevnotfound <student@CS-Mac-mini-18.local>
2025-09-18 12:50:12 -0600

    -Add Barry Goldwater sprite and Q W E R movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float qPos = -2.43f;
+    private float wPos = -0.91f;
+    private float ePos = 0.9f;
+    private float rPos = 2.64f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            pos = new Vector3(qPos, pos.y, pos.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.W)) {
+            pos = new Vector3(wPos, pos.y, pos.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.E)) {
+            pos = new Vector3(ePos, pos.y, pos.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.R)) {
+            pos = new Vector3(rPos, pos.y, pos.z);
+        }
+
+        transform.position = pos;
     }
 }
