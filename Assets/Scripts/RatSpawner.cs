fc790102498049ee41777c36d74c0fc2b020531b
404kevnotfound <student@CS-Mac-mini-18.local>
2025-10-16 14:56:45 -0600

    -Add variable and slowly ramping rat spawning

--- Assets/Scripts/RatSpawner.cs
+++ Assets/Scripts/RatSpawner.cs
@@ -6,20 +6,38 @@ public class RatSpawner : MonoBehaviour
     public Transform[] spawnPoints;
     public GameManager gameManager;
 
+    // rat speed variables
     public float globalSpeedMultiplier = 1f;
     public float speedIncreaseRate = 0.05f;
     public float maxGlobalSpeedMultiplier = 5f;
 
+    // rat spawn variables
+    private float spawnTimer;
+    public float spawnInterval = 3f;
+    public float minSpawnInterval = 0.5f;
+    public float spawnIncreaseRate = 0.003f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating(nameof(SpawnRat), 1f, 2f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // handle global speed
         globalSpeedMultiplier = Mathf.Lerp(1f, maxGlobalSpeedMultiplier, Mathf.Clamp01(Time.timeSinceLevelLoad / 120f));
+
+        // handle spawn timing
+        spawnTimer += Time.deltaTime;
+
+        if(spawnTimer >= spawnInterval) {
+            SpawnRat();
+            spawnTimer = 0f;
+        }
+
+        spawnInterval = Mathf.Max(spawnInterval - spawnIncreaseRate * Time.deltaTime, minSpawnInterval);
     }
 
     void SpawnRat() {
